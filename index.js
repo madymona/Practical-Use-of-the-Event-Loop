e881058a79dea9630836c3a128bb1dcd5ed9ee0b
madymona <madymona@gmail.com>
2024-06-13 11:50:14 -0400

    part3

--- index.js
+++ index.js
@@ -12,9 +12,31 @@ function simpleRecursiveFunc() {
   try {
     simpleRecursiveFunc()
   } catch (error) {
-    console.log("Stack overflow occurred after " + counter + " calls.");
-    console.log(error.message);
+    console.log("Stack overflow occurred after " + counter + " calls.")
+    console.log(error.message)
   }
 }
 
 simpleRecursiveFunc()
+
+//Part 2: Trampolines
+/**Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
+Once your recursive function is complete, trampoline it. */
+
+
+//Part 3: Deferred Execution
+/*Create a simple HTML element to hold text. Cache this HTML element into a JavaScript variable.
+Write a function that takes a parameter n and adds a list of all prime numbers between one and n to your HTML element.
+Once complete, use the alert() method to alert the user that the calculation is finished. */
+
+const primeNumbers = document.getElementById('primeNumbers')
+
+function isPrime(n) {
+    if (n <= 1) return false
+    if (n === 2) return true
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n% i === 0) return false
+    }
+    return true
+}
+
